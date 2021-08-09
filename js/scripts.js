b691d70e8abd00f8e100bed36b49e7e98e57227d
o-leandro <leanddrosanttoss@gmail.com>
2021-08-09 19:04:42 -0300

    Criação de responsivo para resolução 576px

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    var btnMenuMobile = $('.btn-menumobile');
+
+    $('.btn-menumobile').on('click', function() {
+        $('.nav-container ul').toggleClass('open');
+    });
+});
